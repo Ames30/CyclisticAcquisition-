ed121824d7e75875cb4fb2473453016a6d4c2feb
Amy Henson <148791826+Ames30@users.noreply.github.com>
2023-11-14 14:59:42 -0700

    fixed some bugs
    still trying to figure out a graph

--- Cyclistic_Q1_Analysis.R
+++ Cyclistic_Q1_Analysis.R
@@ -5,6 +5,7 @@ install.packages("dplyr")
 install.packages("tidyr")
 install.packages("readr")
 install.packages("stringr")
+install.packages("data.table")
 
 #Loading packages for analysis
 library(tidyverse)
@@ -13,27 +14,17 @@ library(dplyr)
 library(tidyr)
 library(readr)
 library(stringr)
+library(data.table)
+library(lubridate)
 
 #Importing and Merging all CSV files into one dataframe
-#getwd()
-#setwd("C:\\Data_Analyst_Coursera\\CSV_Cleaned")
-#Q1<- list.files(path='C:\\Data_Analyst_Coursera\\CSV_Cleaned') %>%
-	lapply(read_csv) %>%
-	bind_rows 
-
-setwd("C:")
-#Jan_2023<-read.csv("C:\\Data_Analyst_Coursera\\CSV_Cleaned\\2023_01-divvy-tripdata_cleaned.csv")
-
-install.packages("data.table")
-library(data.table)
+getwd()
+setwd("C:\\Data_Analyst_Coursera\\CSV_Cleaned")
 temp<-list.files("C:\\Data_Analyst_Coursera\\CSV_Cleaned", full.names=TRUE, pattern="\\.csv$")
 Q1<-rbindlist(lapply(temp,fread),fill=TRUE)
 
 #Transform character data to duration data
-#nulls out the data,no error message?
-#library(lubridate)
-#Q1_new<- Q1[, ride_length:=as.duration(ride_length)]
-
+Q1$ride_dur<-as.duration(hms(Q1$ride_length))
 
 #Reviewing resulting dataframe
 head(Q1)
@@ -42,16 +33,5 @@ ncol(Q1)
 str(Q1)
 
 
-#Creating graphs. 
-#not these graphs, but better, more telling graphs once I get the data in order. 
-#Goal is to have a graph anticipating members for following months. Use smooth plot or freqpoly
-
-ggplot(Q1, aes(x=day_of_week, y=ride_length, color=member_casual)) +
-	geom_point()
-
-ggplot(Q1, aes(x=ride_length, y=day_of_week)) +
-	geom_bar()
-
-ggplot(Q1, aes(x=started_at, y=member_casual)) +
-	geom_freqpoly()
+#Creating graphs
 
